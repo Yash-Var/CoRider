4c50ac504f574d71a225a0563829dff729202672
Yash-Var <yashvarshney7011@gmail.com>
2023-05-14 23:18:32 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import image from "./cell.png";
+import arrow from "./Icon.png";
+import doublearrow from "./icon.png";
+import Profile from "./Profile.png";
+import Text from "./text.png";
+import Dot from "./dot.png";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-header">
+      <div className="header">
+        <div className="time">9:41</div>
+        <div>
+          <img src={image} />
+        </div>
+      </div>
+      <div className="section2">
+        <div className="header2">
+          <div>
+            <img src={arrow} className="arrow" />
+          </div>
+          <div className="trip">Trip 1</div>
+        </div>
+        <div className="doublearrow">
+          <img src={doublearrow} className="darrow" />
+        </div>
+      </div>
+      <div className="section4">
+        <div className="section3">
+          <div>
+            <img src={Profile} className="profile" />
+          </div>
+          <div>
+            <img src={Text} className="text" />
+          </div>
+        </div>
+        <div className="Dot">
+          <img src={Dot} />
+        </div>
+      </div>
     </div>
   );
 }
