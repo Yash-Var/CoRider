a33ce41616cf11fd8b9a4b9ecfc9132bb15e43b2
Yash-Var <yashvarshney7011@gmail.com>
2023-05-14 23:45:14 +0530

    date added

---
+++ src/Chat.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Chat.css";
+const Chat = () => {
+  return (
+    <div className="main">
+      <div className="line">
+        <hr className="text2" />
+        <div className="text3">12 JAN,2023</div>
+        <hr className="text2" />
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
